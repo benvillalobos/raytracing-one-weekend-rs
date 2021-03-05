b7fb20490a05e71a470ebf9e5bb4a719c1228d96
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2021-03-04 20:32:06 -0800

    Gamme correction via sqrt

--- src/main.rs
+++ src/main.rs
@@ -63,9 +63,9 @@ fn write_color(color: Vector3<f64>, samples_per_pixel: f64) {
 
     // Divide color by number of samples
     let scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r *= scale.sqrt();
+    g *= scale.sqrt();
+    b *= scale.sqrt();
 
     println!("{} {} {}", (256.0 * clamp(r, 0.0, 0.999)) as i32, (256.0 * clamp(g, 0.0, 0.999)) as i32, (256.0 * clamp(b, 0.0, 0.999)) as i32);
 }
